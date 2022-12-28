2e9a16248c58ede3b2c41f8b5f719ee66fffa1c7
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2022-12-28 23:13:25 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Arrays {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter the length of array");
+	int arr[] = new int[scan.nextInt()];
+	System.out.println("Enter the elements");
+	for (int i = 0; i <= arr.length-1; i++) {
+		System.out.print("element"+i+" is: ");
+		arr[i]=scan.nextInt();
+	}
+	System.out.println("array elements are");
+	for (int i = 0; i <= arr.length-1; i++) {
+		System.out.print(arr[i]+" ");
+	}
+	System.out.println();
+	
+	int sum=0;
+	
+	for (int i = 0; i <= arr.length-1; i++) {
+		sum=sum+arr[i];
+	}
+	System.out.println("sum of all elements"+ sum);
+}
+}
