90472e41b56157cb40645c1354795d154259bd0a
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-01-30 14:29:44 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,11 @@
+package com.progarm3;
+
+public class Program {
+public static void main(String[] args) {
+	for (int i=1; i<=5; i++)
+	{
+		System.out.println("Sachin");
+		System.out.println("Tendulkar");
+	}
+}
+}
