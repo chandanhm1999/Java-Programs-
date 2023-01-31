90439278eef464ba2e4fedc77b695ca2031ed670
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-01-31 20:11:21 +0530

    Add files via upload

---
+++ ProgramDoWhile.java
@@ -0,0 +1,13 @@
+package com.NovemberBatch;
+
+public class ProgramDoWhile {
+public static void main(String[] args) {
+	int i=1;
+	do
+	{
+		System.out.println("Sachin");
+		System.out.println("Tendulkar");
+		i++;
+	}while(i<=5);
+}
+}
