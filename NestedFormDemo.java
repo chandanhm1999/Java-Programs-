03069036f2d0ea20a342dac0bb7868337b8cd12d
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-01-31 20:09:28 +0530

    Add files via upload

---
+++ NestedFormDemo.java
@@ -0,0 +1,18 @@
+package com.NovemberBatch;
+
+public class NestedFormDemo {
+
+	public static void main(String[] args) {
+		for(int j=1; j<=3; j++)
+		{
+			System.out.println("j-->"+j);
+				for(int i=1; i<=7; i++)
+			{
+				System.out.println("i-->"+i+" ");
+				System.out.println("KodNest");
+			}
+				System.out.println("===============");
+		}
+	}
+
+}
