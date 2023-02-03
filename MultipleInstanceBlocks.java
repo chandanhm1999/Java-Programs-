b8abdb678badf83874d3801698a4da1640c071be
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-02-03 22:22:04 +0530

    Add files via upload

---
+++ MultipleInstanceBlocks.java
@@ -0,0 +1,29 @@
+package ujan18;
+
+public class MultipleInstanceBlocks {
+static
+{
+	System.out.println("Hello");
+}
+static 
+{
+	System.out.println("Welcome");
+}
+static
+{
+	System.out.println("To");
+}
+static
+{
+	System.out.println("Kodnest");
+}
+{
+	System.out.println("Technologies");
+}
+{
+	System.out.println("Pvt");
+}
+public static void main(String[] args) {
+System.out.println("Ltd");
+}
+}
