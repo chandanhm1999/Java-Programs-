38bdb4f334ec996c0da877336426d67e9fd03cf8
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-02-07 20:27:44 +0530

    Add files via upload

---
+++ Strings22Ex2p.java
@@ -0,0 +1,11 @@
+package strings22P;
+
+public class Strings22Ex2p {
+public static void main(String[] args) {
+	String s1 = "Rama";
+	System.out.println(s1);
+	String s2 = s1.concat("Sita");
+	System.out.println(s1);
+	System.out.println(s2);
+}
+}
