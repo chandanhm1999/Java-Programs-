c3aececa2859965ea8bdcf33048a2e3a6800c0be
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-02-15 22:01:18 +0530

    Add files via upload

---
+++ Demoo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Demoo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Press The Number");
+		int a = scan.nextInt();
+		if (a==1) {
+			System.out.println("You have pressed-1");
+		}
+		System.out.println("You have not pressed-1");
+	}
+
+}
