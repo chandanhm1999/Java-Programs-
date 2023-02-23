41552198c20bef1e80019acdec2c646517fe8d42
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-02-23 22:25:28 +0530

    Add files via upload

---
+++ Jan4Array.java
@@ -0,0 +1,13 @@
+package jan4pro;
+
+public class Jan4Array {
+public static void main(String[] args) {
+	int arr[]= {10, 20, 30, 40, 50};
+	for (int i = 0; i <= arr.length; i++) {
+		if(i!=3)
+		{
+			System.out.println(arr[i]+ " ");
+		}
+	}
+}
+}
