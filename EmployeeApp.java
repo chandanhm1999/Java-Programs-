aba27d3d93d71901512dda8d00ba3cf2a9cb1841
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-02-24 20:22:12 +0530

    Add files via upload

---
+++ EmployeeApp.java
@@ -0,0 +1,10 @@
+package jan9program;
+
+public class EmployeeApp {
+public static void main(String[] args) {
+	EmployeeExtendsObject e1 = new EmployeeExtendsObject();
+	System.out.println(e1.id);
+	System.out.println(e1.height);
+	System.out.println(e1.name);
+}
+}
