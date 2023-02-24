74720599a5223b6277d8dd12a9c28367a2fd27f8
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-02-24 20:21:20 +0530

    Add files via upload

--- Demo1.java
+++ Demo1.java
@@ -1,28 +1,14 @@
-package com.progarm3;
-
-import java.util.Scanner;
+package jan9program;
 
 public class Demo1 {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter any number between 1-4");
-		int n = scan.nextInt();
-		switch (n)
-		{
-		case 1:
-			System.out.println("You have entered-1");
-			break;
-		case 2:
-			System.out.println("You have entered-2");
-			break;
-		case 3:
-			System.out.println("You have entered-3");
-			break;
-		case 4:
-			System.out.println("You have entered-4");
-			break;
-		default:
-			System.out.println("idiot....See the msg correctly");
-		}
+public static void main(String[] args) {
+	Demo d = new Demo();
+	d.a = 200;
+	d.b = 400.0f;
+	d.c = "Ramu";
+	System.out.println(d.a);
+	System.out.println(d.b);
+	System.out.println(d.c);
+	d.display(100f);
 }
 }
