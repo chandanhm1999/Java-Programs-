3ceca9884995667f024e08867981b4a2a59f2a45
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-02-25 23:14:26 +0530

    Add files via upload

--- Demo1.java
+++ Demo1.java
@@ -1,14 +1,64 @@
-package jan9program;
+package kodnestClass;
+
+import java.util.Scanner;
 
 public class Demo1 {
 public static void main(String[] args) {
-	Demo d = new Demo();
-	d.a = 200;
-	d.b = 400.0f;
-	d.c = "Ramu";
-	System.out.println(d.a);
-	System.out.println(d.b);
-	System.out.println(d.c);
-	d.display(100f);
+	Scanner scan = new Scanner(System.in);
+	while(true) //while loop!
+	{
+	System.out.println("Press-1 for Addition");
+	System.out.println("Press-2 for Subtraction");
+	System.out.println("Press-3 for Multiplication");
+	System.out.println("Press-4 for Division");
+	System.out.println("other Number------>Exit");
+	int input=scan.nextInt();
+	System.out.println(input);
+	int a=0;
+	int b=0;
+	int c=0;
+	
+	switch(input) //switch statement for calculator!
+	{
+	case 1:
+	//Addition
+	System.out.println("Enter 2 Numbers");
+	a=scan.nextInt();
+	b=scan.nextInt();
+	c =a+b;
+	System.out.println(c);
+		break;
+	
+	case 2:
+	//Subtraction
+		System.out.println("Enter 2 Numbers");
+		a=scan.nextInt();
+		b=scan.nextInt();
+		c =a-b;
+		System.out.println(c);
+		break;
+	
+	case 3:
+	//Multiplcation
+		System.out.println("Enter 2 Numbers");
+		a=scan.nextInt();
+		b=scan.nextInt();
+		c =a*b;
+		System.out.println(c);
+		break;
+	
+	case 4:
+	//Divide
+		System.out.println("Enter 2 Numbers");
+		a=scan.nextInt();
+		b=scan.nextInt();
+		c =a/b;
+		System.out.println(c);
+		break;
+		
+	default:
+		return;
+	}
+	}
 }
 }
