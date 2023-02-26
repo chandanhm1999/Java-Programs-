f099cc630a93eb428a3472f795f9a29e746c2a92
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-02-26 20:09:37 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,14 @@
+package kodnestClass;
+
+public class Demo3 {
+public static void main(String[] args) {
+	for (int i = 1; i < 5; i++) {
+		if(i==2)
+		{
+			return;
+		}
+		System.out.println("Kodnest");
+	}
+	System.out.println("Technologies");
+}
+}
