de7a7ff34c5e52ab4434e4e2435a82a2a97e412a
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-02-27 11:43:08 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,19 @@
+package kodnestClass;
+
+public class Loop {
+public static void main(String[] args) {
+	Chandan:for(int i=1; i<=3; i++)
+	{
+		Navanesh:for(int j=1; j<=3; j++)
+		{
+			if(j==2)
+			{
+				System.out.println("Kodnest");
+				break Chandan;
+			}
+		}
+		System.out.println("--------------------");
+	}
+	System.out.println("Technologies");
+}
+}
