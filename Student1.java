afd4f98d01409c2af0de16923fa64eb0b8d27d95
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-03-04 18:09:59 +0530

    Add files via upload

---
+++ Student1.java
@@ -0,0 +1,20 @@
+package kodnestClass;
+
+public class Student1 {
+	int id;
+	String name;
+	int age;
+	String gender;
+	void eat()
+	{
+		System.out.println("Student is Eating & Studing");
+	}
+	void sleep()
+	{
+		System.out.println("Student is Sleeping");
+	}
+	void study()
+	{
+		System.out.println("Student is Studing");
+	}
+}
