2103317a9aeef6515461b63394ce39e1143fd0c6
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-03-04 18:10:17 +0530

    Add files via upload

---
+++ StudentDemo.java
@@ -0,0 +1,18 @@
+package kodnestClass;
+
+public class StudentDemo {
+	int id = 001;
+	String name = "Somya";
+	String gender = "Female";
+	int marks = 35;
+	void eat() {
+		System.out.println("Student is Eating!");
+	}
+	void sleep() {
+		System.out.println("Student is Sleeping!");
+	}
+	void study() {
+		System.out.println("Student is Studing!");
+	}
+}
+
