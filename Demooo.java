1c28089c0ce84ffabb0c55c8036b943a7d1690a2
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-03-11 20:58:35 +0530

    Add files via upload

---
+++ Demooo.java
@@ -0,0 +1,13 @@
+package com.project;
+
+public class Demooo {
+
+	public static void main(String[] args) {
+		int a = 220;
+		int b = 30;
+		int c = ++a + ++a + a++ + b;
+		int d = c + b + a;
+		System.out.println(d);
+	}
+
+}
