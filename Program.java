35a65882ce841b721429ca54dc030388cd89a6da
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-03-15 19:48:00 +0530

    Add files via upload

--- Program.java
+++ Program.java
@@ -1,17 +1,11 @@
-package DecArray30prbS;
-
-import java.util.Scanner;
+package com.progarm3;
 
 public class Program {
 public static void main(String[] args) {
-	int arr[] = {1,2,4,5,6};
-	Scanner scan = new Scanner(System.in);
-	System.out.println("Enter the length of array");
-	System.out.println("Enter the elements");
-	for (int i = 0; i <= arr.length-1; i++) {
-		System.out.print("element"+i+" is: ");
-		arr[i]=scan.nextInt();
+	for (int i=1; i<=5; i++)
+	{
+		System.out.println("Sachin");
+		System.out.println("Tendulkar");
 	}
-	System.out.println("array elements are");
 }
 }
