7d337b35e96b6c8e19f23e3ee9f9a793dbeed59f
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-03-23 20:30:56 +0530

    Add files via upload

--- StudentApp.java
+++ StudentApp.java
@@ -1,11 +1,8 @@
-package ujanpro;
+package ujan10pro;
 
 public class StudentApp {
-public static void main(String[] args) {
-	Student res = Student.getStudentInstance();
-	res.eat();
-	res.sleep();
-	res.study();
-	System.out.println(res);
-}
+	public static void main(String[] args) {
+      Student s=new Student("Ramu",123 ,70);
+      System.out.println(s.name+" "+s.id+" "+s.marks);
+	}
 }
