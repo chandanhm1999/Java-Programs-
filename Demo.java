1b70ef2b0c8c914d4cd0fd0b1fc5c347253aa3fd
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-04-07 22:13:44 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,10 +1,30 @@
+package kodnest15d;
+
 import java.util.Scanner;
 
 public class Demo {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter a number");
-		int a=scan.nextInt();
-		System.out.println(a);
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter Array length to create 1-D  Array");
+	Person1 []arr = new Person1[scan.nextInt()];
+	for (int i = 0; i <= arr.length-1; i++) {
+		arr[i] = new Person1();
 	}
+	for (int i = 0; i <= arr.length-1; i++) {
+		System.out.println("Enter name for Person"+i);
+		arr[i].name = scan.next();
+		System.out.println("Enter age for Person"+i);
+		arr[i].age = scan.nextInt();
+		System.out.println("Enter gender for Person"+i);
+		arr[i].gender = scan.next();
+		System.out.println("Enter phoneno for Person"+i);
+		arr[i].phone = scan.nextInt();
+		System.out.println("Enter address for Person"+i);
+		arr[i].address = scan.next();
+	}
+	System.out.println("Array Contents are");
+	for (int i = 0; i <= arr.length-1; i++) {
+		System.out.print(arr[i].name + " " + arr[i].age + " " + arr[i].gender + " " +arr[i].phone + " " + arr[i].address + " " + "|");
+	}
+}
 }
