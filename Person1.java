02629fc2eb93388416dc9bc8ee26b388ee6cc1a1
CHANDAN HM <109410990+chandanhm1999@users.noreply.github.com>
2023-04-07 22:15:15 +0530

    Add files via upload

---
+++ Person1.java
@@ -0,0 +1,17 @@
+package kodnest15d;
+
+public class Person1 {
+	String name;
+	int age;
+	String gender;
+	int phone;
+	String address;
+	void eat()
+	{
+		System.out.println("Person is Eating");
+	}
+	void sleep()
+	{
+		System.out.println("Person is Sleeping");
+	}
+}
